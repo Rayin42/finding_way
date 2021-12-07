213624904c3b8574e9470cf4f78f372cf6596ba1
Rayin42 <dgjosh@naver.com>
2021-12-07 17:59:58 +0900

    공백 정리, 문법 정리 중

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import time, win32con, win32api, win32gui, ctypes
 import pyperclip
 import pandas as pd
-from apscheduler.schedulers.background import BackgroundScheduler
 
 
-#채팅방 이름
+# 채팅방 이름
 kakao_open_chat = '샆'
 discord_open_chat = "떠돌이｜상인 - Discord"
-#검색할 단어
+# 검색할 단어
 command1 = '전설'
 command2 = '웨이'
-#최신 데이터를 다루기 위해 이전 기록을 저장
+# 최신 데이터를 다루기 위해 이전 기록을 저장
 global cls
 
-#특수키 입력 등을 위한 세팅
+# 특수키 입력 등을 위한 세팅
 PBYTE256 = ctypes.c_ubyte * 256
 _user32 = ctypes.WinDLL("user32")
 GetKeyboardState = _user32.GetKeyboardState
@@ -30,8 +29,9 @@ MapVirtualKeyW = _user32.MapVirtualKeyW
 MakeLong = win32api.MAKELONG
 w = win32con
 
-#카카오톡이 활성되어 있을 때 채팅방 검색, 그 창을 띄움
-#핸들을 찾아갈 때 친구검색 -> 채팅방 검색 순으로 이루어져야 하기 때문에 edit2_1 -> edit2_2순으로 해야할 필요가 있었음
+
+# 카카오톡이 활성되어 있을 때 채팅방 검색, 그 창을 띄움
+# 핸들을 찾아갈 때 친구검색 -> 채팅방 검색 순으로 이루어져야 하기 때문에 edit2_1 -> edit2_2순으로 해야할 필요가 있었음
 def open_chat(chat_name):
     hwnd_kakao = win32gui.FindWindow(None, "카카오톡")
     hwnd_kakao_edit1 = win32gui.FindWindowEx(hwnd_kakao, None, "EVA_ChildWindow", None)
@@ -45,7 +45,7 @@ def open_chat(chat_name):
     time.sleep(1)
 
 
-#카카오톡 채팅방 -> 글자입력 파트를 찾아 메시지를 입력
+# 카카오톡 채팅방 -> 글자입력 파트를 찾아 메시지를 입력
 def send_kakaotext(chat_name, text):
     hwnd_main = win32gui.FindWindow(None, chat_name)
     hwnd_edit = win32gui.FindWindowEx(hwnd_main, None, "RICHEDIT50W", None)
@@ -54,7 +54,7 @@ def send_kakaotext(chat_name, text):
     send_return(hwnd_edit)
 
 
-#엔터 역할
+# 엔터 역할
 def send_return(hwnd):
     win32api.PostMessage(hwnd, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
     time.sleep(0.01)
@@ -62,22 +62,14 @@ def send_return(hwnd):
 
 
 def copy_chat(chat_name):
-    #오류 방지용 클립보드 내용 덮어쓰기
+    # 오류 방지용 클립보드 내용 덮어쓰기
     pyperclip.copy(" ")
-    #디스코드 이름으로 윈도우 핸들을 찾아내 ctrl A, ctrl C로 모든 대화내용 복사
+    # 디스코드 이름으로 윈도우 핸들을 찾아내 ctrl A, ctrl C로 모든 대화내용 복사
     hwnd_main = win32gui.FindWindow(None, chat_name)
     PostKeyEx(hwnd_main, ord('A'), [w.VK_CONTROL], False)
     time.sleep(1)
     PostKeyEx(hwnd_main, ord('C'), [w.VK_CONTROL], False)
-
-    #테스트 시 test_case
-    # test = "test_case"
-    # hwnd_main = win32gui.FindWindow(None, test)
-    # PostKeyEx(hwnd_main, ord('A'), [w.VK_CONTROL], False)
-    # time.sleep(1)
-    # PostKeyEx(hwnd_main, ord('C'), [w.VK_CONTROL], False)
-
-    #클립보드 내용 붙여넣기
+    # 클립보드 내용 붙여넣기
     ctext = pyperclip.paste()
     return ctext
 
@@ -119,7 +111,7 @@ def PostKeyEx(hwnd, key, shift, specialkey):
             SendMessage(hwnd, msg_up, key, lparam | 0xC0000000)
 
 
-#프로그램을 구동했을 때 이미 진행된 대화를 cls에 저장
+# 프로그램을 구동했을 때 이미 진행된 대화를 cls에 저장
 def save_last_chat():
     count = 0
     while True:
@@ -131,7 +123,6 @@ def save_last_chat():
         count += 1
         if count > 5:
             return -1
-    print(df)
     return df.iloc[-2, 0]
 
 
@@ -143,12 +134,11 @@ def check_chat_command(cls):
         if len(df) > 3:
             break
 
-    #로그 확인용
+    # 로그 확인용
     print(f"{time.localtime().tm_year}-{time.localtime().tm_mon}-{time.localtime().tm_mday} / "
           f"{time.localtime().tm_hour}:{time.localtime().tm_min}:{time.localtime().tm_sec}")
 
-    #기존 저장되어 있던 데이터와 비교, 잘라내서 최신 데이터만 확인할 수 있도록
-    #df_index = df.index[df[0] == cls]
+    # 기존 저장되어 있던 데이터와 비교, 잘라내서 최신 데이터만 확인할 수 있도록
     for i in range(len(df)):
         if df.iloc[-i, 0] == cls:
             df_index = len(df)-i
@@ -156,7 +146,7 @@ def check_chat_command(cls):
 
     df1 = df.iloc[df_index + 1:, 0]
 
-    # #지정한 커맨드가 잘라낸 데이터에 속해 있는지 파악, 1개 이상 있을 경우 카카오톡을 열고 순서대로 발송
+    # 지정한 커맨드가 잘라낸 데이터에 속해 있는지 파악, 1개 이상 있을 경우 카카오톡을 열고 순서대로 발송
     found = df1[df1.str.contains(command1)]
     if int(found.count()) > 0:
         open_chat(kakao_open_chat)
@@ -165,7 +155,7 @@ def check_chat_command(cls):
             time.sleep(0.5)
         print("전호!")
 
-    #커맨드 2번 있는지 파악(여유 되면 나중에 데코레이터를 쓰든 합치든)
+    # 커맨드 2번 있는지 파악(여유 되면 나중에 데코레이터를 쓰든 합치든)
     found = df1[df1.str.contains(command2)]
     if int(found.count()) > 0:
         open_chat(kakao_open_chat)
@@ -177,7 +167,7 @@ def check_chat_command(cls):
     return df.iloc[-2, 0]
 
 
-#스케쥴러 수행명령
+# 스케쥴러 수행명령
 def job_1(clss):
     global cls
     cls = check_chat_command(clss)
@@ -193,12 +183,6 @@ def main():
     print("대응 카카오톡 방 : " + kakao_open_chat)
     print("init copy complete")
 
-    #스케쥴러 사용 시 parameter 변수가 갱신이 되지 않음 -> 시계 구현
-    # sched = BackgroundScheduler(timezone="utc")
-    # sched.start()
-    # sched.add_job(job_1, 'cron', minute="40", second='01', id="test_01", args=[cls])
-    # sched.add_job(job_1, 'cron', minute="45", second='01', id="test_02", args=[cls])
-    #sched.add_job(job_1, 'interval', seconds=3, id="test_03", args=[cls])
     count = 0
     while True:
         time.sleep(1)
